f4b439aa95baf24751ea0095bd3f0d3fd098db2f
Hamilton Kibbe <hamilton.kibbe@gmail.com>
2012-12-13 11:37:11 -0500

    refactor

--- data/dataset.py
+++ data/dataset.py
@@ -2,9 +2,9 @@
 """ dataset.py
 Datasets
 """
-from datafeed import IntradayQuotes
-from datetime import date
 import numpy as np
+from datafeed import IntradayQuotes
+from utilities import get_raw_data
 
 
 class Dataset(object:
@@ -82,10 +82,16 @@ class MLDataset(Dataset):
     def __init__(self, symbols=None, sector=None, index=None):
         """
         """
-        self.data = None
+        
+        # Set up 
         self.target_data = None
         self.meta_data = None
-        self._initialize_dataset(self, symbols=symbols, sector=sector, index=index, size=size)
+        super(MLDataset, self).__init__()
+        
+    def __iter__(self):
+        """ Get an iterator over the dataset
+        """
+        return iter(zip(self.data,self.target_data))
         
     def _initialize_dataset(self, symbols=None, sector=None, index=None, size=None):
         if sector is not None:
@@ -112,16 +118,17 @@ class MLDataset(Dataset):
         # Clean up
         self._sanitize()
 
+
     def get_data(self):
         """ Get the raw data from the dataset
 
         :returns: Data and Labels/target data as a tuple
         """
-        return (self.data, self.target_data)
+        return zip(self.data, self.target_data)
 
     def to_csv(self, filename):
         """ Write dataset to CSV file
-
+        
         :param filename: Name of the file to write.
         :type filename: str
         """
@@ -154,7 +161,7 @@ class MLDataset(Dataset):
     def _sanitize(self):
         """ Clean up datasets
 
-        Removes any rows with empty fields
+        Remove any rows with empty fields or fields of incorrect type...
         """
         delrows = []
         for i in range(len(self.data)):
@@ -177,59 +184,4 @@ class MLDataset(Dataset):
                                    
                                    
                                    
-def get_raw_data(ticker):
-    
-    # Get quotes
-    quotes = IntradayQuotes().get_quotes(ticker,
-                                         date(1900, 01, 01),
-                                         date.today())
-    # Generate matrix
-    return (np.array([np.array(
-        [q.Date.weekday(),
-         q.AdjClose,
-         q.Volume,
-         q.Features.ma_5_day,
-         q.Features.ma_10_day,
-         q.Features.ma_20_day,
-         q.Features.ma_50_day,
-         q.Features.ma_100_day,
-         q.Features.ma_200_day,
-         q.Features.ewma_5_day,
-         q.Features.ewma_10_day,
-         q.Features.ewma_12_day,
-         q.Features.ewma_20_day,
-         q.Features.ewma_26_day,
-         q.Features.ewma_50_day,
-         q.Features.ewma_100_day,
-         q.Features.ewma_200_day,
-         (q.AdjClose - q.Features.ma_5_day)
-            if q.Features.ma_5_day else None,
-         (q.AdjClose - q.Features.ma_10_day)
-            if q.Features.ma_10_day else None,
-         (q.AdjClose - q.Features.ma_20_day)
-            if q.Features.ma_20_day else None,
-         (q.AdjClose - q.Features.ma_50_day)
-            if q.Features.ma_50_day else None,
-         (q.AdjClose - q.Features.ma_100_day)
-            if q.Features.ma_100_day else None,
-         (q.AdjClose - q.Features.ma_200_day)
-            if q.Features.ma_200_day else None,
-         (q.AdjClose - q.Features.ewma_5_day)
-            if q.Features.ewma_5_day else None,
-         (q.AdjClose - q.Features.ewma_10_day)
-            if q.Features.ewma_10_day else None,
-         (q.AdjClose - q.Features.ewma_12_day)
-            if q.Features.ewma_12_day else None,
-         (q.AdjClose - q.Features.ewma_20_day)
-            if q.Features.ewma_20_day else None,
-         (q.AdjClose - q.Features.ewma_50_day)
-            if q.Features.ewma_50_day else None,
-         (q.AdjClose - q.Features.ewma_100_day)
-            if q.Features.ewma_100_day else None,
-         (q.AdjClose - q.Features.ewma_200_day)
-            if q.Features.ewma_200_day else None,
-         q.Features.macd,
-         q.Features.macd_signal,
-         q.Features.macd_histogram])
-        for q in quotes]).astype(float),
-        np.array([np.array([q.Ticker, q.Date]) for q in quotes]))
+
---
+++ data/utilities.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+""" utilities.py
+Utility functions
+"""
+import numpy as np
+from datafeed import IntradayQuotes
+from datetime import date
+
+def get_raw_data(ticker, start=date(1900, 01, 01), end=date.today()):
+    """ Generate an array of quotes and indicators for the given stock
+    """
+    # Get quotes
+    quotes = IntradayQuotes().get_quotes(ticker, start, end)
+    # Generate matrix
+    return (np.array([np.array(
+        [q.Date.weekday(),
+         q.AdjClose,
+         q.Volume,
+         q.Features.ma_5_day,
+         q.Features.ma_10_day,
+         q.Features.ma_20_day,
+         q.Features.ma_50_day,
+         q.Features.ma_100_day,
+         q.Features.ma_200_day,
+         q.Features.ewma_5_day,
+         q.Features.ewma_10_day,
+         q.Features.ewma_12_day,
+         q.Features.ewma_20_day,
+         q.Features.ewma_26_day,
+         q.Features.ewma_50_day,
+         q.Features.ewma_100_day,
+         q.Features.ewma_200_day,
+         (q.AdjClose - q.Features.ma_5_day)
+            if q.Features.ma_5_day else None,
+         (q.AdjClose - q.Features.ma_10_day)
+            if q.Features.ma_10_day else None,
+         (q.AdjClose - q.Features.ma_20_day)
+            if q.Features.ma_20_day else None,
+         (q.AdjClose - q.Features.ma_50_day)
+            if q.Features.ma_50_day else None,
+         (q.AdjClose - q.Features.ma_100_day)
+            if q.Features.ma_100_day else None,
+         (q.AdjClose - q.Features.ma_200_day)
+            if q.Features.ma_200_day else None,
+         (q.AdjClose - q.Features.ewma_5_day)
+            if q.Features.ewma_5_day else None,
+         (q.AdjClose - q.Features.ewma_10_day)
+            if q.Features.ewma_10_day else None,
+         (q.AdjClose - q.Features.ewma_12_day)
+            if q.Features.ewma_12_day else None,
+         (q.AdjClose - q.Features.ewma_20_day)
+            if q.Features.ewma_20_day else None,
+         (q.AdjClose - q.Features.ewma_50_day)
+            if q.Features.ewma_50_day else None,
+         (q.AdjClose - q.Features.ewma_100_day)
+            if q.Features.ewma_100_day else None,
+         (q.AdjClose - q.Features.ewma_200_day)
+            if q.Features.ewma_200_day else None,
+         q.Features.macd,
+         q.Features.macd_signal,
+         q.Features.macd_histogram])
+        for q in quotes]).astype(float),
+        np.array([np.array([q.Ticker, q.Date]) for q in quotes]))
