80e09b3089820ec43a41dedf1dd739b59999dae8
Hamilton Kibbe <ham@hamiltonkibbe.com>
2012-12-13 20:31:22 -0500

    ...

--- database/indicators.py
+++ database/indicators.py
@@ -10,15 +10,17 @@ def find_needs_updating(data, length):
     """ Get list of missing data
     """
     to_update = array([x for x in where(isnan(data))[0] if x >= (length - 1)])
-    return (to_update, {'min': min(to_update), 'max': max(to_update), 'len': length})
-
-def is_up_to_date(ticker, col_name):
+    if len(to_update) > 0:
+        return (to_update, {'min': min(to_update), 'max': max(to_update), 'len': length})
+    else:
+        return (to_update, {'min': 0, 'max': 0, 'len': length})
+def is_up_to_date(ticker, col_name, session):
     last = (session.query(Quote).filter_by(Ticker=ticker)
                                 .order_by(Quote.Date.desc())
                                 .first())
     return getattr(last.Features, col_name) is not None
-        
-def get_column(ticker, col_name):
+
+def get_column(ticker, col_name, session):
     return asarray(zip(*[(q.Id, q.AdjClose, getattr(q.Features, col_name))
                          for q in (session.query(Quote)
                                           .filter_by(Ticker=ticker)
@@ -28,17 +30,17 @@ def start(range_data):
     """ Get starting index
     """
     return (range_data['min'] - (range_data['len'] - 1))
-    
-def end(range_data)
+
+def end(range_data):
     """ Get ending index
     """
     return (range_data['max'] + 1)
-                                          
+
 def calc_index(index, range_data):
     """ Map calculated index
     """
     return index + (range_data['len'] - (range_data['min'] + 1))
-    
+
 def get_dataset(ticker, session, *columns):
     """ Get a numpy ndarray containing the specified columns
     TODO: Make this work
@@ -67,24 +69,22 @@ def update_ma(ticker, length, session, commit=True, check_all=False):
     """
     ticker = ticker.lower()
     col_name = 'ma_' + str(length) + '_day'
-    
-    if not check_all and is_up_to_date(ticker, col_name):
+
+    if not check_all and is_up_to_date(ticker, col_name, session):
         return
         #last = (session.query(Quote).filter_by(Ticker=ticker)
         #                            .order_by(Quote.Date.desc())
         #                            .first())
         #if getattr(last.Features, col_name) is not None:
         #    return
-            return
-        
-        
+
     #data = asarray(zip(*[(q.Id, q.AdjClose, getattr(q.Features, col_name))
     #                     for q in (session.query(Quote)
     #                                      .filter_by(Ticker=ticker)
     #                                      .all())]))
-    data = get_column(ticker, col_name)
+    data = get_column(ticker, col_name, session)
 
-     
+
     ids = data[0]
     adj_close = data[1].astype(float)
     ma = data[2].astype(float)
@@ -123,15 +123,15 @@ def update_ewma(ticker, length, session, commit=True, check_all=False):
     """
     ticker = ticker.lower()
     col_name = 'ewma_' + str(length) + '_day'
-    
-    if not check_all and is_up_to_date(ticker, col_name):
+
+    if not check_all and is_up_to_date(ticker, col_name, session):
         return
 
-    data = get_column(ticker, col_name)
+    data = get_column(ticker, col_name, session)
     ids = data[0]
     adj_close = data[1].astype(float)
     ma = data[2].astype(float)
-    
+
     to_update, range = find_needs_updating(ma, length)
     if len(to_update) > 0:
         calc = analysis.exp_weighted_moving_average(adj_close[start(range):end(range)],
@@ -230,18 +230,18 @@ def update_macd(ticker, session, commit=True, check_all=False):
     macd = data[3].astype(float)
 
     #to_update = array([x for x in where(isnan(macd))[0] if x >= 25])
-    to_update, _min, _max = find_needs_updating(macd, length)
+    to_update, range_d = find_needs_updating(macd, length)
     if len(to_update) > 0:
-        #_min = min(to_update)
-        #_max = max(to_update)
+        _min = range_d['min']
+        _max = range_d['max']
         macd = analysis.macd(fast_ewma=fast_ewma[_min - 8:_max + 1],
                              slow_ewma=slow_ewma[_min - 8:_max + 1])
         macd_signal = analysis.macd_signal(macd=macd)
         macd = macd[8:]
         macd_signal = macd_signal[8:]
         for idx in to_update:
-            calc_macd = macd[idx - _min]
-            calc_macd_sig = macd_signal[idx - _min]
+            calc_macd = macd[idx - range_d['min']]
+            calc_macd_sig = macd_signal[idx - range_d['min']]
             (session.query(Indicator)
                     .filter_by(Id=ids[idx])
                     .update({'macd': calc_macd,
