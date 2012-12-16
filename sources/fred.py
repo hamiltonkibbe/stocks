a66476167eb61f626a8a92086fed04cd29e3d221
Hamilton Kibbe <ham@hamiltonkibbe.com>
2012-12-16 15:18:55 -0500

    switch to fred api

--- sources/fred.py
+++ sources/fred.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
-#
-#  This is based on Corey Goldberg's (corey@goldb.org) ystockquote module, with 
-#  additional api methods exposed
-# 
-#
-#  Copyright 2012 Hamilton Kibbe (hamilton.kibbe@gmail.com)
-#
-#  license: GNU LGPL
-#
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2.1 of the License, or (at your option) any later version.
+""" fred.py
 
+Python interface to the St.Louis Fed's Federal Reserve Economic Data
+"""
 
 import urllib
+from xml.etree import ElementTree
 from datetime import date
 from time import mktime, strptime
 
-indicators = {'bank_prime_loan_rate': 'WPRIME.txt',
+indicators = {'bank_prime_loan_rate': 'WPRIME',
               'consumer_price_index': 'SOMETHING'}
 
 
+FRED_API_KEY = '34a7386da77efa763957d30a167376aa'
+
+
 def _get_url(fname):
-    return 'http://research.stlouisfed.org/fred2/data/' + fname
+    return 'http://api.stlouisfed.org/fred/series/observations?series_id=' + fname + '&api_key=' + FRED_API_KEY
 
 def _get_raw(fname):
-    raw =[i.strip().split() for i in urllib.urlopen(_get_url(fname)).readlines()]
-    raw = raw[raw.index(['DATE','VALUE']) + 1:]
-    return {'dates':[date.fromtimestamp(mktime(strptime(row[0],'%Y-%m-%d')))  for row in raw],'values': [float(row[1]) for row in raw]}
+    tree = ElementTree.fromstring(urllib.urlopen(_get_url(fname)).read())
+    observations = tree.iter('observation')
+    return {'dates': [date.fromtimestamp(mktime(strptime(obs.get('date'), '%Y-%m-%d'))) for obs in tree.iter('observation')],
+            'values': [float(obs.get('value')) for obs in tree.iter('observation')]}
 
 def get(indicator):
     return _get_raw(indicators[indicator])
