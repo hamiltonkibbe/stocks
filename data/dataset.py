8c9be9c358fda7db079a7927df4f73c7d9577988
Hamilton Kibbe <ham@hamiltonkibbe.com>
2012-12-21 22:51:44 -0500

    fix dataset init

--- data/dataset.py
+++ data/dataset.py
@@ -10,10 +10,10 @@ from utilities import get_raw_data
 class Dataset(object):
     """ Dataset Class
     """
-    def __init__(self, symbols=None, sector=None, 
+    def __init__(self, symbols=None, sector=None,
                  index=None, size=None, data_callback = None):
         """ Create an instance of the Dataset class
-        
+
         :param symbols: List of securities to include in dataset
         :param sector: List of sectors to include in dataset
         :param index: List of indicies to include in dataset
@@ -24,21 +24,21 @@ class Dataset(object):
         """
         self.data = None
         self.col_names = None
-        self._initialize_dataset(symbols, sector, index, size, stock_callback)
+        self._initialize_dataset(symbols, sector, index, size, data_callback)
 
     @property
     def pretty_data(self):
         """ Data from dataset with column headers
         """
         return np.vstack((self.col_names, self.data))
-    
+
     @property
     def raw_data(self):
-        """ Raw data from dataset 
+        """ Raw data from dataset
         """
         return self.data
-        
-        
+
+
     def to_csv(self, filename):
         """ Write Dataset to CSV file
         """
@@ -52,11 +52,11 @@ class Dataset(object):
         if symbols is not None:
             for ticker in symbols:
                 data, col_names = get_raw_data(ticker)
-                
+
                 # Do callback
                 if data_callback:
                     data_callback(data)
-                    
+
                 # Add each row to dataset
                 if self.data is None:
                     self.data = data
@@ -69,7 +69,7 @@ class Dataset(object):
             pass
         if size is not None:
             pass
-        
+
     def _sanitize(self):
         """ Clean up datasets
         Removes any rows with empty fields
@@ -101,37 +101,37 @@ class Dataset(object):
 
 class MLDataset(Dataset):
     """ Dataset for Machine Learning
-    
+
     Data set that provides training and target data for machine learning
     """
     def __init__(self, symbols=None, sector=None, index=None, size=None, target_function=None):
         """ Create an instance of the MLDataset class
-        
+
         :param symbols: List of securities to include in dataset
         :param sector: List of sectors to include in dataset
         :param index: List of indicies to include in dataset
         :param size: Maximum number of rows to include in dataset
-        :param target_function: function that generates target data for machine 
-        learning / regression. The function should take a 2D numpy array and 
+        :param target_function: function that generates target data for machine
+        learning / regression. The function should take a 2D numpy array and
         return a 1D numpy array.
         """
         # Initialize class
         self.target = None
         self.generate_target_data = self._generate_callback(target_function)
         super(MLDataset, self).__init__(symbols, sector, index, size, self.generate_target_data)
-    
+
     @property
     def training_data(self):
         """ Training dataset for regression / machine learning
         """
         return np.array(self.data[:][2:]).astype(float)
-        
+
     @property
     def target_data(self):
         """ Target data for regression / machine learning
         """
         return self.target.astype(float)
-        
+
 
     def _sanitize(self):
         """ Clean up datasets
@@ -146,22 +146,22 @@ class MLDataset(Dataset):
             for val in self.data[i]:
                 if (not isinstance(val, float)) or (val is None) or (not np.isfinite(val)):
                     delrow = True
-                    
+
             if (self.target[i] is None) or (not np.isfinite(self.target[i])):
                 delrow = True
-                
+
             if delrow:
                 delrows.append(i)
 
         # Remove rows marked for deletion
         self.data = np.delete(self.data, delrows, 0)
         self.target = np.delete(self.target, delrows, 0)
-                                   
+
 
     def _generate_callback(self, target_function):
         """ wrap the user function that creates the target data for training
-        
-        Generate a function that appends the output from the user supplied 
+
+        Generate a function that appends the output from the user supplied
         function to the target_data array.
         """
         def _function(data):
@@ -171,7 +171,7 @@ class MLDataset(Dataset):
                 self.target = np.append(self.target, target_function(data))
         return _function
 
-        
+
     def __iter__(self):
         """ Get an iterator over the dataset
         """
