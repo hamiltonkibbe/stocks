3a0b79f6903b9e855b1f84778401e445d7f69726
Hamilton Kibbe <ham@hamiltonkibbe.com>
2013-07-27 00:19:21 -0400

    fix google finance user agent

--- sources/googlefinance.py
+++ sources/googlefinance.py
@@ -13,13 +13,17 @@ This module provides a Python API for retrieving stock data from Google Finance.
 
 """
 
+# Google doesn't like Python's user agent...
+class FirefoxOpener(urllib.FancyURLopener):
+    version = 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
 
 def __request(symbol):
     url = 'http://google.com/finance/historical?q=%s&output=csv' % symbol
-    return urllib.urlopen(url).read().strip().strip('"')
+    opener = FirefoxOpener()
+    return opener.open(url).read().strip().strip('"')
 
 
-def get_historical_prices(symbol, start_date, end_date):
+def get_historical_prices(symbol, start_date=None, end_date=None):
     """
     Get historical prices for the given ticker symbol.
 
