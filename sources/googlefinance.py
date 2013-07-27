3c44bff18db62b8d1e21c6cabff9195e003a3e75
Hamilton Kibbe <ham@hamiltonkibbe.com>
2013-07-27 00:00:35 -0400

    added google finance interface

---
+++ sources/googlefinance.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+#  Copyright 2013 Hamilton Kibbe (hamilton.kibbe@gmail.com)
+#
+
+
+import urllib
+from datetime import date
+
+""" googlefinance
+
+This module provides a Python API for retrieving stock data from Google Finance.
+
+"""
+
+
+def __request(symbol):
+    url = 'http://google.com/finance/historical?q=%s&output=csv' % symbol
+    return urllib.urlopen(url).read().strip().strip('"')
+
+
+def get_historical_prices(symbol, start_date, end_date):
+    """
+    Get historical prices for the given ticker symbol.
+
+    Returns a nested list.
+    """
+    return [data.split() for data in __request(symbol).split('\n')]
+
+
